f04501648376b42fedda72627e8168c4e51e40af
Israel Mejia <0325110069@ut-tijuana.edu.mx>
2026-04-02 18:44:10 -0700

    V14

--- lab.js
+++ lab.js
@@ -1,4 +1,7 @@
 
+
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+
 let scene, camera, renderer, controls;
 let geometry, material, currentMesh;
 let light;
